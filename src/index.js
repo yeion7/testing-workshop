8de703f9e3634f7785717a959361efd4f569970e
Yeison Daza <jahir045@gmail.com>
2018-06-15 20:51:09 -0500

    remove: useless files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import "antd/dist/antd.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
