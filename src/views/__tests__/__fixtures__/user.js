0e49798ede3e630d3650c78cf4a8b5713c80da86
Yeison Daza <yeison@resuelvetudeuda.com>
2018-07-14 15:17:27 -0500

    b

---
+++ src/views/__tests__/__fixtures__/user.js
@@ -0,0 +1,22 @@
+export const userData = {
+  id: "9",
+  createdAt: "2018-06-12T10:03:54.651Z",
+  name: "Quinten Collins",
+  avatar:
+    "https://s3.amazonaws.com/uifaces/faces/twitter/davidmerrique/128.jpg",
+  description: "Customer",
+  followers_count: 48287,
+  listed_count: 86604,
+  location: "Kutchside",
+  screen_name: "Terry48",
+  tweets: [
+    {
+      id: "3",
+      userId: "9",
+      created_at: "2017-11-03T17:19:14.896Z",
+      text: "Coordinator",
+      retweet_count: 79329,
+      favorite_count: 17
+    }
+  ]
+};
