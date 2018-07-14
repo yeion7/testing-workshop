27452a64206e448b1c8b67abc50c2d313846dbed
Yeison Daza <yeison@resuelvetudeuda.com>
2018-07-14 15:03:13 -0500

    a

--- src/components/__tests__/ModalNewUser.test.js
+++ src/components/__tests__/ModalNewUser.test.js
@@ -27,4 +27,25 @@ describe('ModalNewUser', () => {
     fireEvent.click(getByText('Crear'))
     expect(onOk).toHaveBeenCalled();
   });
+
+  test('should works', () => {
+    // Render 
+    const onOk = jest.fn()
+    const {debug, getByLabelText, getByText} = 
+      render(<ModalNewUser visible onOk={onOk} />)
+
+    // Acciones
+    
+    const user = getByLabelText('Usuario')
+    user.value = 'Usuario'
+    fireEvent.change(user)
+
+    const description = getByLabelText('Descripción')
+    description.value = 'Descripción'
+    fireEvent.change(description)
+
+    fireEvent.click(getByText('Crear'))
+    expect(getByText('Ingresa un nombre!')).toBeTruthy();
+    expect(onOk).not.toHaveBeenCalled();
+  });
 });
