42fd5e5abadca9f431f1d7cf52a0f148f032c61c
Yeison Daza <yeison@resuelvetudeuda.com>
2018-07-14 15:29:08 -0500

    d

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,25 @@
 import 'react-testing-library/cleanup-after-each'
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+window.localStorage = new LocalStorageMock();
